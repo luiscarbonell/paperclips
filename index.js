32904882cbbd3f5988f9464b9ccfea0be5143bbf
Luis Carbonell <luiscarbonell5683@gmail.com>
2018-07-17 07:21:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ let m_p = setInterval(function() { document.getElementById('btnMakePaperclip').c
 
 // Buy Wire
 let b_w = setInterval(function() { if(document.getElementById('wire').innerHTML === "0") document.getElementById('btnBuyWire').click()  }, 100)
+
+// Complete Project
+let c_p = setInterval(function() { let a = document.getElementById('projectListTop').querySelectorAll('button:not([disabled])'); if(a.length > 0) (a[0]).click(); }, 500)
