5c9b524a0c047dcc209ceca2bbda6250685a0a08
Luis Carbonell <luiscarbonell5683@gmail.com>
2018-07-17 07:23:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Make Paperclips
-let m_p = setInterval(function() { document.getElementById('btnMakePaperclip').click() }, 10)
+let m_p = setInterval(function() { document.getElementById('btnMakePaperclip').click() }, 1)
 
 // Buy Wire
 let b_w = setInterval(function() { if(document.getElementById('wire').innerHTML === "0") document.getElementById('btnBuyWire').click()  }, 100)
 
 // Complete Project
-let c_p = setInterval(function() { let a = document.getElementById('projectListTop').querySelectorAll('button:not([disabled])'); if(a.length > 0) (a[0]).click(); }, 500)
+let c_p = setInterval(function() { let a = document.getElementById('projectListTop').querySelectorAll('button:not([disabled])'); if(a.length > 0) (a[0]).click(); }, 1000)
